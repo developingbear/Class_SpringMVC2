f74901e3d43749ea8458f2fc277bc6acda432795
developingbear <developingbear@gmail.com>
2021-07-20 16:01:09 +0900

    class: thymeleaf errorCode and Message 01 ~ 06

---
+++ validation/src/test/java/hello/itemservice/validation/MessageCodesResolverTest.java
@@ -0,0 +1,39 @@
+package hello.itemservice.validation;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.validation.DefaultMessageCodesResolver;
+import org.springframework.validation.MessageCodesResolver;
+import org.springframework.validation.ObjectError;
+
+public class MessageCodesResolverTest {
+
+    MessageCodesResolver codesResolver = new DefaultMessageCodesResolver();
+
+    @Test
+    void messageCodesResolverObject(){
+        String[] strings = codesResolver.resolveMessageCodes("required", "item");
+        for (String string : strings) {
+            System.out.println("string = " + string);
+        }
+        /*
+        내부적으로 실행되는 code
+        new ObjectError("item", new String[]{"required.item", "required"};
+        */
+        Assertions.assertThat(strings).containsExactly("required.item", "required");
+    }
+
+    @Test
+    void messageCodesResolverField(){
+        String[] messageCodes = codesResolver.resolveMessageCodes("required", "item", "itemName", String.class);
+        for (String messageCode : messageCodes) {
+            System.out.println("messageCode = " + messageCode);
+        }
+        Assertions.assertThat(messageCodes).containsExactly(
+                "required.item.itemName",
+                "required.itemName",
+                "required.java.lang.String",
+                "required"
+        );
+    }
+}
