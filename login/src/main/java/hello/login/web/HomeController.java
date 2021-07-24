aa0872772ade9f7b133b349b6e18219270e9fd13
developingbear <developingbear@gmail.com>
2021-07-24 23:07:23 +0900

    class: thymeleaf Login - Session Spring ~ Session timeout

--- login/src/main/java/hello/login/web/HomeController.java
+++ login/src/main/java/hello/login/web/HomeController.java
@@ -9,8 +9,10 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.CookieValue;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.SessionAttribute;
 
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
 import java.lang.management.LockInfo;
 
 @Slf4j
@@ -44,7 +46,7 @@ public class HomeController {
     }
 
     //session 사용
-    @GetMapping("/")
+    //@GetMapping("/")
     public String homeLoginV2(HttpServletRequest request, Model model) {
 
         Member member = (Member)sessionManager.getSession(request);
@@ -57,5 +59,35 @@ public class HomeController {
         return "loginHome";
 
     }
+    //@GetMapping("/")
+    public String homeLoginV3(HttpServletRequest request, Model model) {
 
+        HttpSession session = request.getSession(false);
+        if (session == null) {
+            return "home";
+        }
+
+        Member member = (Member)session.getAttribute(SessionConst.LOGIN_MEMBER);
+        if (member == null) {
+            return "home";
+        }
+
+        model.addAttribute("member", member);
+        return "loginHome";
+
+    }
+
+    @GetMapping("/")
+    public String homeLoginV3Spring(
+            @SessionAttribute(name = SessionConst.LOGIN_MEMBER, required = false) Member member
+            , Model model) {
+
+        if (member == null) {
+            return "home";
+        }
+
+        model.addAttribute("member", member);
+        return "loginHome";
+
+    }
 }
