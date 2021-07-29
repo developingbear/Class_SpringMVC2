b0973f822f066089334919fdd8ceeecb4741107e
developingbear <developingbear@gmail.com>
2021-07-29 17:01:23 +0900

    class: thymeleaf Error - API Error : @ControllerAdvice

---
+++ exception/src/main/java/hello/exception/advice/ExControllerAdvice.java
@@ -0,0 +1,38 @@
+package hello.exception.advice;
+
+import hello.exception.exception.UserException;
+import hello.exception.exhandler.ErrorResult;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@Slf4j
+@RestControllerAdvice(basePackages = "hello.exception.api")
+public class ExControllerAdvice {
+
+    @ExceptionHandler(value = IllegalArgumentException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST) // 이 값을 붙이지않으면 200 OK가 나가기 때문에 오해가 생길수 있다.
+    public ErrorResult illegalExHandler(IllegalArgumentException e) {
+        log.error("[exceptionHandler]: {}", e.getClass());
+        return new ErrorResult("BAD", e.getMessage());
+    }
+
+    @ExceptionHandler // method pram에 exception을 적어주면 어노테이션에서는 생략이 가능하다.
+    public ResponseEntity<ErrorResult> userExHandler(UserException e) {
+        log.error("[exceptionHandler]: {}", e.getClass());
+        ErrorResult errorResult = new ErrorResult("USER-EX", e.getMessage());
+        return new ResponseEntity(errorResult, HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    public ErrorResult exHandler(Exception e) {
+        log.error("[exceptionHandler]: {}", e.getClass());
+        return new ErrorResult("ex", e.getMessage());
+    }
+
+
+}
